fc6aa78b21f65469cce7743b4a2efa295541a025
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-15 14:00:10 +0200

    persist local matrix updates to database

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import getCurrentMatrixReducer from './getCurrentMatrix';
+import updateDatabaseMatrixReducer from './updateDatabaseMatrix';
 
-export default combineReducers({ getCurrentMatrixReducer });
+export default combineReducers({ getCurrentMatrixReducer, updateDatabaseMatrixReducer });
