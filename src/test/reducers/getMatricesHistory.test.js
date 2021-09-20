8c8fb3e57affea4b00e4d8339ed20f2087a1c1fb
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-20 15:37:54 +0200

    get matrices history reducer

---
+++ src/test/reducers/getMatricesHistory.test.js
@@ -0,0 +1,56 @@
+import getMatricesHistoryReducer from '../../redux/reducers/getMatricesHistory';
+
+describe(getMatricesHistoryReducer, () => {
+  const initialState = {
+    matrices: [{
+      matrix: [],
+      updated_at: '',
+    }],
+  };
+
+  const action = {
+    type: 'GET_HISTORY_SUCCESS',
+    payload: {
+      matrices: [
+        {
+          matrix: [{
+            id: 1,
+            learning_outcome: 'Testing redux',
+            skills_level: 1,
+            theme: {
+              title: 'Automated testing',
+              link: 'https://github.com/abcd',
+            },
+            apprenticeship_level: 1,
+          }],
+
+          updated_at: '',
+        },
+      ],
+    },
+  };
+  it('should return initialState', () => {
+    expect(getMatricesHistoryReducer(undefined, {})).toEqual(initialState);
+  });
+
+  it('should return the history of matrices', () => {
+    expect(getMatricesHistoryReducer(undefined, action)).toEqual({
+      matrices: [
+        {
+          matrix: [{
+            id: 1,
+            learning_outcome: 'Testing redux',
+            skills_level: 1,
+            theme: {
+              title: 'Automated testing',
+              link: 'https://github.com/abcd',
+            },
+            apprenticeship_level: 1,
+          }],
+
+          updated_at: '',
+        },
+      ],
+    });
+  });
+});
