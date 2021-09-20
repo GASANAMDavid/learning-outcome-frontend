bb296e762b863d11ab8b47c1823e3b3750022d5a
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-20 15:37:54 +0200

    get matrices history reducer

---
+++ src/redux/reducers/getMatricesHistory.js
@@ -0,0 +1,19 @@
+const initialState = {
+  matrices: [{
+    matrix: [],
+    updated_at: '',
+  }],
+};
+const getMatricesHistoryReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'GET_HISTORY_SUCCESS':
+    return {
+      ...state,
+      matrices: action.payload.matrices,
+    };
+  default:
+    return state;
+  }
+};
+
+export default getMatricesHistoryReducer;
