6f233d5182fa2cdb4a619e0b34020435b2838f67
David Gasana Manzi <gmdavid59@gmail.com>
2021-09-24 15:24:08 +0200

    rendering different matrices from the history

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import getCurrentMatrixReducer from './getCurrentMatrix';
 import updateMatrixReducer from './updateMatrix';
 import getMatricesHistoryReducer from './getMatricesHistory';
+import setVersionIdToBeDisplayedReducer from './setVersionIDToBeDisplayed';
 
 export default combineReducers({
   getCurrentMatrixReducer,
   updateMatrixReducer,
   getMatricesHistoryReducer,
+  setVersionIdToBeDisplayedReducer,
 });
--- src/test/Components/dashboard/Version.test.js
+++ src/test/Components/dashboard/Version.test.js
@@ -1,9 +1,14 @@
 import { shallow } from 'enzyme';
 import React from 'react';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import Version from '../../../components/dashboard/Version';
 
 describe(Version, () => {
+  const mockStore = configureStore([thunk]);
+  const store = mockStore({});
   it('renders correctly', () => {
-    shallow(<Version />);
+    shallow(<Provider store={store}><Version /></Provider>);
   });
 });
