e4d2c1514b7c63e05c5be6a20deed1c69decd235
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-05 15:53:34 +0200

    create a new user at auth0 and use redux

---
+++ src/redux/actions/createUser.js
@@ -0,0 +1,37 @@
+import axiosInstance from '../../helpers/api';
+import setSnackbar from './snackbar';
+import store from '../store/store';
+import Auth from '../../helpers/auth';
+
+const createUserStart = () => (
+  {
+    type: 'CREATE_USER_START',
+  }
+);
+
+export const addUserInfo = (payload) => (
+  {
+    type: 'ADD_USER_INFO',
+    payload,
+  }
+);
+
+const createUser = () => (dispatch) => {
+  const { createUserReducer: { user: existingUser } } = store.getState();
+  const body = {
+    first_name: existingUser.first_name,
+    last_name: existingUser.last_name,
+    email: existingUser.email,
+  };
+  dispatch(createUserStart());
+  axiosInstance.post('/user', body)
+    .then(({ data }) => {
+      dispatch(setSnackbar(data.message));
+      new Auth().signIn({ email: existingUser.email, password: existingUser.password });
+    })
+    .catch(({ error }) => {
+      console.log(error);
+    });
+};
+
+export default createUser;
