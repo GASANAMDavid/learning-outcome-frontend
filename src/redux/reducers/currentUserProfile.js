692d5cde5da251288e284207d4ee42584b8bc323
David Gasana Manzi <gmdavid59@gmail.com>
2021-10-18 09:09:11 +0200

    current user profile reducer

---
+++ src/redux/reducers/currentUserProfile.js
@@ -0,0 +1,26 @@
+const initialState = {
+  isLoading: false,
+  profile: { first_name: '', last_name: '', email: '' },
+};
+const currentUserProfileReducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'GET_CURRENT_USER_SUCCESS':
+    return {
+      ...state,
+      profile: action.payload.user,
+      isLoading: false,
+    };
+  case 'UPDATE_USER_PROFILE':
+    return {
+      ...state,
+      profile: {
+        ...state.profile,
+        ...action.payload,
+      },
+    };
+  default:
+    return state;
+  }
+};
+
+export default currentUserProfileReducer;
